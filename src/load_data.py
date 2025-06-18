923ab69c2984312f526e241cabf9c5f1e39b36a3
Metasebiya Akililu <metibizu@gmail.com>
2025-06-18 15:31:35 +0300

    "chore: create a cleaned data loading function"

--- src/load_data.py
+++ src/load_data.py
@@ -35,6 +35,27 @@ class DataLoader:
         print(df.head())
         return df
 
+    def load_cleaned_data(self, file_path):
+        """
+        Load a CSV file into a pandas DataFrame for financial analysis
+
+        Parameters:
+            file_path (str): Path to the CSV file (e.g., 'data/raw/{app_name}.csv')
+
+        Returns:
+            pd.DataFrame: Loaded customer review data as a DataFrame
+
+        Raises:
+            FileNotFoundError: If the specified file does not exist
+        """
+
+        if not os.path.isfile(file_path):
+            raise FileNotFoundError(f"File {file_path} does not exist")
+        df = pd.read_csv(file_path)
+        print(df.head())
+        return df
+
+
 if __name__ == "__main__":
     df = "../data/raw/MachineLearningRating_v3.txt"
 
